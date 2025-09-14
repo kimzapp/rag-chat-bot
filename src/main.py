8979be834ab8c1847c278d63b94e3c319f991fa1
zap <hoangkimgiap_t67@hus.edu.vn>
2025-09-14 13:18:06 +0700

    test: streamlit demo for chat feature

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from vectorstore import get_vector_store_from_config
-from configs import get_config
-
-vector_store = get_vector_store_from_config()
-print(f"Using vector store: {type(vector_store).__name__}")
-print(vector_store.config)
